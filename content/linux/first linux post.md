d0255a1ce9860b19ca654694720bc6aaffe2662a
Guohua Ouyang <ouyanggh0815@gmail.com>
2015-04-14 08:06:22 +0800

    rebuilding site Tue Apr 14 08:06:22 CST 2015

--- content/linux/first linux post.md
+++ content/linux/first linux post.md
@@ -1,7 +1,7 @@
 ---
 date: "2015-04-14T06:26:59+08:00"
 draft: true
-title: "First go"
+title: "linux posts"
 
 ---
 
---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project. 
+hugo -t hyde # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1   ]
+          then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Push master
+git checkout master
+rsync -avrztpg public/* ./
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1   ]
+          then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
