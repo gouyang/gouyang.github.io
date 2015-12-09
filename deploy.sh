be88e22beb67a437489bf101154d47bd5c57970a
Guohua Ouyang <gouyang@redhat.com>
2015-12-09 12:50:39 +0800

    fix error for step back

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo -t hyde # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Add changes to git.
-# git add -A
+git add -A
 
 # Commit changes.
-# msg="rebuilding site `date`"
-# if [ $# -eq 1   ]
-#          then msg="$1"
-# fi
-# git commit -m "$msg"
+msg="rebuilding site `date`"
+if [ $# -eq 1   ]
+         then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
 git push 
